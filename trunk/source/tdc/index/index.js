81c086917fe610a4cf39207cd36d8829ca76b14a
lelezhangzhao <lelezhangzhao@163.com>
2018-09-20 17:04:05 +0800

    backup

---
+++ trunk/source/tdc/index/index.js
@@ -0,0 +1,10 @@
+Page({
+  data:{
+    imgUrls:["../image/1.jpg", "../image/2.jpg", "../image/3.jpg"],
+    indicatorDots:true,
+    autoplay:true,
+    circular:true,
+    displayMultiipleItems:3,
+  }
+  
+})
