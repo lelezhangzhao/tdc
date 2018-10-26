a8e5091cf964d24ae702cefc57fd978b51c747da
lelezhangzhao <lelezhangzhao@163.com>
2018-10-26 08:58:04 +0800

    bakcup

--- trunk/source/tdc/component/briefIntroduction/briefIntroduction.js
+++ trunk/source/tdc/component/briefIntroduction/briefIntroduction.js
@@ -1,13 +1,33 @@
 // tdc/component/briefIntroduction/briefIntroduction.js
+var GlobalData = require("../../util/globaldata.js");
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
+    "logo":{
+      type: String,
+      value: "../../image/global/logo.png",
+    },
+    "isServerLogo":{
+      type: Boolean,
+      value: true,
+    },
+
     "tags":{
       type: Array,
-      value: ["拉丁","全职"]
+      value: ["拉丁","全职"],
+    },
+
+    "briefIntroduction":{
+      type: String,
+      value: "个人简介",
+    },
+    "isTeacher":{
+      type: Boolean,
+      value: false,
     }
+
 
   },
 
@@ -22,6 +42,17 @@ Component({
    * 组件的方法列表
    */
   methods: {
+
+  },
+  ready: function(){
+    var that = this;
+    var isServerLogo = that.properties.isServerLogo;
+    if (isServerLogo){
+      var logo = that.properties.logo;
+      that.setData({
+        logo: GlobalData.GetServerHttps() + "static/image/logo/" + logo,
+      })
+    }
 
   }
 })
---
+++ trunk/source/tdc/util/globaldata.js
@@ -0,0 +1,10 @@
+let Server = "https://localtdc.com/";
+
+function GetServerHttps(){
+  return Server;
+}
+
+
+module.exports = {
+  GetServerHttps: GetServerHttps
+} 
